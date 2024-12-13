0a9ad4e1a61170d04a4f821168454967893b44c0
JoshEmbedded <joshua.schiff.2000@gmail.com>
2024-12-13 16:43:05 +0100

    Point transformation to base frame.

--- alpaka_demo/src/alpaka_pose_test.py
+++ alpaka_demo/src/alpaka_pose_test.py
@@ -40,33 +40,61 @@ def transform_point(sensor_point, rotation_matrix, translation_vector):
 if __name__ == '__main__':
     
     robot_pose = []
-    robot_pose.append(-0.54826)
-    robot_pose.append(-0.50286)
-    robot_pose.append(0.70163)
+    robot_pose.append(-0.81873)
+    robot_pose.append(-0.28587)
+    robot_pose.append(0.84057)
+    
+    robot_rpy = [0, 0, 0.362]
     # rx = 0.221
     # ry = 0.518
     # rz = -0.024
     
-    point_x = 0.00854
+    #First method: Point is from sensor to point
+    point_x = -0.01166
     point_x = -point_x #x axis is inversed on sensor plane
     point_y = 0 #only scans along x,z plane
-    point_z = 0.135
+    point_z = 0.1176
+    point_z = point_z
     point_scanned = [point_x, point_y, point_z]
     
+    
+    #Second method: where the offset is considers going from sensor to tcp frame
+    # point_x = -0.01166
+    # point_x = -point_x #x axis is inversed on sensor plane
+    # point_y = 0 #only scans along x,z plane
+    # point_z = 0.1176
+    # point_z = -point_z
+    # point_scanned = [point_x, point_y, point_z]
+    
+    #First attempt where the offset is from TCP frame to sensor
     offset_x = 0
-    offset_y = 0.07237
-    offset_z = -0.08944 #calculated from offset on plan diagram
+    offset_y = 0.07192
+    offset_z = -0.11511 #calculated from offset on plan diagram
+    
+    #Second Method
+    # offset_x = 0
+    # offset_y = -0.07192
+    # offset_z = 0.11511 #calculated from offset on plan diagram
     
     sensor_to_TCP_translation = [offset_x, offset_y, offset_z]
-    sensor_to_TCP_rotation = R.from_euler('x', 16.5, degrees=True)
+    
+    sensor_to_TCP_rotation = R.from_euler('x', 16.1, degrees=True)
+    
     sensor_to_TCP_rotation = sensor_to_TCP_rotation.as_matrix()
     
-    transformed_point = transform_point(point_scanned, sensor_to_TCP_rotation, sensor_to_TCP_translation)
+    transformed_point_tcp_frame = transform_point(point_scanned, sensor_to_TCP_rotation, sensor_to_TCP_translation)
+    print("Transformed Point in TCP Frame:", transformed_point_tcp_frame)
+    
+    tcp_to_base_rotation = R.from_euler('xyz', robot_rpy)
+    tcp_to_base_rotation = tcp_to_base_rotation.as_matrix()
+    tcp_to_base_translation = [robot_pose[0], robot_pose[1], robot_pose[2]]
+    
+    transformed_point = transform_point(transformed_point_tcp_frame, tcp_to_base_rotation, tcp_to_base_translation)
     print("Relative point to robot pose:", transformed_point)
-    robot_pose[0]+=transformed_point[0]
-    robot_pose[1]+=transformed_point[1]
-    robot_pose[2]+=transformed_point[2]
-    print("Updated robot position:", robot_pose )
+    # robot_pose[0]+=transformed_point[0]
+    # robot_pose[1]+=transformed_point[1]
+    # robot_pose[2]+=transformed_point[2]
+    # print("Updated robot position:", robot_pose )
     # print(sensor_to_TCP_rotation)
     
     # rotation = R.from_euler('xyz', [rx, ry, rz], degrees=False)
